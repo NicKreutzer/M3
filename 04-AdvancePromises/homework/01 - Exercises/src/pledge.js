8fd706f01889a1c4a136e4d8599507157f13d4c6
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-03-10 21:47:50 -0300

    Terminar

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -3,60 +3,69 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
-class $Promise{
-    constructor(executor){
-        if(typeof executor !== 'function') throw TypeError('Executor must be a function')
-        this._state = 'pending';
-        this._value = undefined;
-        this._handlerGroups = [];
+function $Promise (executor) {
+    if(typeof executor !== "function") 
+    throw TypeError("executor must be function");
 
-        const resolve = (value) => {this._internalResolve(value)};
-        const reject = (reason) => {this._internarlReject(reason)};
+    this._state = "pending";
+    this._value = undefined;
+    this._handlerGroups = [];
 
-        executor(resolve, reject)
-    }
+    executor(this._internalResolve.bind(this), this._internalReject.bind(this));
 
-    _internalResolve(value){
-        if(this._state !== 'pending') return;
-        this._state = 'fullfiled';
-        this._value = value;
-        this._callHandlers (this._value);
-    };
-
-    _internarlReject(reason){
-        if(this._state !== 'pending') return;
-        this._state = 'rejected';
-        this._value = reason;
-        this._callHandlers (this._value);
-    };
-
-    then(successCb, errorCb){
-        const handlerGroup = {
-            successCb: typeof successCb === 'function' 
-            ? successCb 
-            : null,                                 //! Tambien puede ser false o 0.
-            errorCb: typeof errorCb === 'function' 
-            ? errorCb 
-            : null,                                 //! Tambien puede ser false o 0.
-        }
-        this._handlerGroups.push(handlerGroup);     //! Guardamos el obj de grupos de handler en el array handlerGroups.
-        this._state !== 'pending' && this._callHandlers(this._value)  //! Si el estado es distinto a pendiente ejecuto callHandlers.
-    };
-
-    _callHandlers(value){
-        while(this._handlerGroups.length){
-            const currentHandler = this._handlerGroups.shift();
-            this._state === 'fullfiled' && currentHandler.successCb && currentHandler.successCb(value)
-            //! Si esto es true             y esto es true               se ejecuta esto.
-            this._state === 'rejected' && currentHandler.errorCb && currentHandler.errorCb(value)
-        }
-    };
-
-    catch(errorHandler){
-        this.then(null, errorHandler)
-    };
 };
 
+$Promise.prototype._internalResolve = function (value) {
+    if(this._state === "pending") {
+        this._state = "fulfilled";
+        this._value = value;
+        this._callHandlers()}
+};
+
+$Promise.prototype._internalReject = function (reason) {
+if(this._state === "pending"){
+    this._state = "rejected";
+    this._value = reason;
+    this._callHandlers()}
+};
+
+$Promise.prototype.then = function (successCb, errorCb){
+    // if(typeof successCb !== "function") successCb = false;
+    // if(typeof errorCb !== "function") errorCb = false;
+    const downstreamPromise = new Promise (() => {})
+    this._handlerGroups.push({
+        successCb: typeof successCb === "function" ? successCb : false,
+        errorCb: typeof errorCb === "function" ? errorCb : false,
+        downstreamPromise: downstreamPromise
+        // successCb: successCb,
+        // errorCb: errorCb,
+        // downstreamPromise: downstreamPromise
+    });
+    if(this._state !== "pending") this._callHandlers();
+    return downstreamPromise;                           //! Con esto falla => .catch devuelve lo mismo que .then
+};
+
+$Promise.prototype._callHandlers = function(){
+    while(this._handlerGroups.length){
+        const hd = this._handlerGroups.shift();
+        // if(this._state === "fulfilled"){
+        //     if(hd.successCb){
+        //         hd.successCb(this._value)
+        //     }
+        // }
+        this._state === "fulfilled" && hd.successCb && hd.successCb(this._value)
+        // else {
+        //     if(hd.errorCb){
+        //         hd.errorCb(this._value)
+        //     }
+        // }
+        this._state === "rejected" && hd.errorCb && hd.errorCb(this._value)
+    }
+};
+
+$Promise.prototype.catch = function (errorCb){
+    return this.then(null, errorCb);
+};
 
 
 module.exports = $Promise;
