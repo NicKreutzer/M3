044a71a27673ba5e19bc6032fd511fb1d90572b9
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-03-10 23:02:00 -0300

    04

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -54,6 +54,35 @@ server.get('/posts/:author', (req, res) => {
     } else {
         res.status(400).json({error: "No existe ninguna publicación del autor indicado"})
     }
+
+    //! ASI DEBERIA SER SI NO ESTUVIESE MAL EL TEST.
+    // const {author} = req.params;
+    // if(author){
+    //     const filteredPosts = publications.filter(post =>
+    //         post.author === author)
+    //         filteredPosts.length
+    //         ? res.status(200).json(filteredPosts)
+    //         : res.status(404).json({error: "El autor no se encuentra en nuestra base de datos"})
+    // } else {
+    //     return res.status(400).json({error: "El autor no fue brindado"})
+    // }
 });
+
+server.put('/posts/:id', (req, res) => {
+    const {id} = req.params;
+    const {title, contents} =  req.body;
+    if(title && contents && id){
+        const publicationId = publications.find(post => post.id === Number(id))
+        !publicationId
+        ? res.status(400).json({error: "No se recibió el id correcto necesario para modificar la publicación"})
+        : (publicationId = {
+            ...publicationId, 
+            title, 
+            contents}
+            && res.status(200).json(publicationId))
+    }else{
+        return res.status(400).json({error: "No se recibieron los parámetros necesarios para modificar la publicación"})
+    }
+})
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
