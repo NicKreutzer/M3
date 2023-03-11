2ddf814393a1f8bbeb7343174ae61ad40ef1bf8a
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-03-10 22:10:37 -0300

    01

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 
 let publications = [];
@@ -5,6 +6,29 @@ let publications = [];
 const server = express();
 
 server.use(express.json());
+
+let id = 1;
+
+server.post('/posts', (req, res) => {
+    const {author, title, contents} = req.body;
+
+    // if(!author || !title || !contents){
+    //     return res.status(400).json({error: "No se recibieron los parámetros necesarios para crear la publicación"})
+    // }
+    if(author && title && contents){
+        const publication = {
+            author,
+            title,
+            contents,
+            id: id++
+        }
+        publications.push(publication);
+        res.status(200).json(publication)
+    } else{
+        return res.status(400).json(
+            {error: "No se recibieron los parámetros necesarios para crear la publicación"})
+    }
+})
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
